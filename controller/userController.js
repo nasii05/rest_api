7a1bffd4eb9fad3db8e2aabc91e659504e513955
nasii05 <iamnasih@gmail.com>
2023-09-22 00:08:09 +0530

    added get and post methods

--- controller/userController.js
+++ controller/userController.js
@@ -1,23 +1,29 @@
 import User from '../models/userModel.js';
 
-export const fetch = (req, res) => {
+export const fetch = async (req, res) => {
     try {
-        
+        const users = await User.find();
+        if (users.length === 0) {
+            return res.status(404).json({ message: "users not found" })
+        }
+        res.status(200).json(users);
     } catch (error) {
-        
+        res.status(500).json({ error: "Internal Server Error" });
     }
 }
 
 
 export const create = async (req, res) => {
     try {
-       const userData = new User(req.body);
-       const {email} = userData;
-       const userExist = await User.findOne({email}); 
-       if(userExist){
-          return res.status(400).json({message: 'user already exists!'})
-       }
+        const userData = new User(req.body);
+        const { email } = userData;
+        const userExist = await User.findOne({ email });
+        if (userExist) {
+            return res.status(400).json({ message: 'user already exists!' })
+        }
+        const savedUser = await userData.save();
+        res.status(200).json(savedUser);
     } catch (error) {
-        res.status(500).json({error: "Internal Server Error"});
+        res.status(500).json({ error: "Internal Server Error" });
     }
 }
